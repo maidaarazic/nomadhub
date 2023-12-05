f8cc2023ab001c7db791896addc4fc5a72d64032
maidaarazic <75145162+maidaarazic@users.noreply.github.com>
2023-12-05 01:31:17 +0100

    Update PlaceTest.java

--- PlaceTest.java
+++ PlaceTest.java
@@ -9,27 +9,21 @@ public class PlaceTest {
         URL url = new URL("http://localhost:8080/api/places/add");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        // Set up the request
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setDoOutput(true);
 
-        // Set the JSON payload
         String jsonInputString = "{ \"name\": \"Coffee Shop\", \"location\": \"Downtown\" }";
 
-        // Send the request
         try (OutputStream os = connection.getOutputStream()) {
             byte[] input = jsonInputString.getBytes("utf-8");
             os.write(input, 0, input.length);
         }
 
-        // Get the response
         int responseCode = connection.getResponseCode();
         System.out.println("Response Code: " + responseCode);
 
-        // Read the response, if needed
-        // ...
-
+        
         connection.disconnect();
     }
 }
