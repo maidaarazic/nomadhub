852a3cc79d3eaa6fa907ee56ade5cfbe9b693c02
maidaarazic <75145162+maidaarazic@users.noreply.github.com>
2023-12-25 11:34:05 +0100

    Update ForgotPassword.jsx

--- ForgotPassword.jsx
+++ ForgotPassword.jsx
@@ -1,27 +1,95 @@
 import React, { useState } from 'react';
-import firebase from '../firebase';
+import axios from 'axios';
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleForgotPassword = async () => {
     try {
-      await firebase.auth().sendPasswordResetEmail(email);
-     
-      console.log('Password reset email sent successfully');
+      const response = await axios.post('http://localhost:8080/api/users/forgot-password', {
+        email,
+      });
+
+      setMessage(response.data.message);
     } catch (error) {
       console.error(error.message);
-      
+      setMessage('An error occurred. Please try again.');
     }
   };
 
   return (
-    <div>
-      <h2>Forgot Password</h2>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <button onClick={handleForgotPassword}>Reset Password</button>
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Forgot Password</h1>
+      <p style={styles.paragraph1}>Enter your email to reset your password:</p>
+      <input
+        style={styles.input}
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={handleForgotPassword} style={styles.button}>
+        <b>Reset Password</b>
+      </button>
+      {message && <p style={styles.message}>{message}</p>}
+      <p style={styles.paragraph1}>
+        Remember your password? <a href="/Login" style={styles.link}>
+          Login
+        </a>
+      </p>
     </div>
   );
 };
+
+const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      backgroundColor: '#001F3F',
+    },
+    heading: {
+      fontSize: '60px',
+      marginBottom: '40px',
+      color: '#fff',
+    },
+
+    input: {
+      marginBottom: '30px',
+      fontSize: '15px',
+      padding: '10px', 
+      height: '20px',
+      borderRadius: '5px',
+    },
+  
+    button: {
+      padding: '10px 20px',
+      fontSize: '18px',
+      backgroundColor: '#a30029',
+      color: '#FFF',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    },
+    paragraph1: {
+      fontSize: '15px',
+      color: '#fff',
+      textAlign: 'center',
+    },
+    paragraph2: {
+      fontSize: '15px',
+      margin: '40px 30px',
+      color: '#fff',
+      textAlign: 'center',
+    },
+
+    link: {
+      color: '#a30029',
+      fontWeight: 'bold',
+    },
+  };
 
 export default ForgotPassword;
