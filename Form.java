97de49ab946f73c43fc9115eb144415b4c9107b2
maidaarazic <75145162+maidaarazic@users.noreply.github.com>
2024-01-19 12:36:31 +0100

    Create Form.java

---
+++ Form.java
@@ -0,0 +1,49 @@
+
+public class Form {
+    private String name;
+    private String location;
+    private String category;
+    private boolean wifi;
+    private boolean powerOutlets;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public boolean isPowerOutlets() {
+        return powerOutlets;
+    }
+
+    public void setPowerOutlets(boolean powerOutlets) {
+    	
+    }
+    
+}
