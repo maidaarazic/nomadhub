80b2b78a3995b0f69661daaaf7c52062d8538986
maidaarazic <75145162+maidaarazic@users.noreply.github.com>
2024-01-21 21:10:48 +0100

    Update Place.java

--- Place.java
+++ Place.java
@@ -1,8 +1,24 @@
 package com.example.demo;
 
 public class Place {
-	
-	private String name;
-	private String location;
+    private String name;
+    private String location;
 
+   
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+   
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
