18fb4e0c0b3e542ba92b5a11415bd9e80907be97
maidaarazic <75145162+maidaarazic@users.noreply.github.com>
2024-01-21 20:56:17 +0100

    Update AddPage.jsx

--- AddPage.jsx
+++ AddPage.jsx
@@ -1,38 +1,38 @@
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import background from './images/close-up-man-typing-laptop.png';
 
-const AddPage = () => {
+const AddPage = ({ onAddPlaceClick }) => {
   return (
     <div style={styles.container}>
-      <h2 style={styles.heading}>Add a Place</h2>
-      <p style={styles.text}>Didn't find your place? Add it now.</p>
-      <Link to="/add-place">
-        <button style={styles.button}>Add Place</button>
-      </Link>
+      <h2 style={styles.heading}>You own a place and want to promote it? Or did you have an amazing experience working at a particular place and want more people to know about it? Add it now!</h2>
+      <button style={styles.button} onClick={() => onAddPlaceClick('Login')}>
+        <b>Add Place</b>
+      </button>
     </div>
   );
 };
 
 const styles = {
   container: {
-    textAlign: 'center',
-    padding: '20px',
-    backgroundColor: '#001F3F',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundImage: `url(${background})`,
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: "no-repeat",
   },
   heading: {
-    fontSize: '24px',
-    margin: '10px 0',
-  },
-  text: {
-    fontSize: '16px',
-    color: '#555', 
+    fontSize: '25px',
+    margin: '30px 45px',
+    color: '#FFF',
   },
   button: {
     padding: '10px 20px',
     fontSize: '18px',
-    backgroundColor: '#a30029', 
-    color: '#FFF', 
+    backgroundColor: '#a30029',
+    color: '#FFF',
     border: 'none',
     borderRadius: '5px',
     cursor: 'pointer',
